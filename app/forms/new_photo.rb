24e3ebc2083035b1e4bc8f3ebb713d96a230d5cc
Huang Chuan <river1440>
2022-06-15 10:45:54 +0800

    feat: add view function for photo

--- app/forms/new_photo.rb
+++ app/forms/new_photo.rb
@@ -9,9 +9,8 @@ module DFans
 
       params do
         required(:filename).filled(max_size?: 256, format?: FILENAME_REGEX)
-        required(:image_data).maybe(:string)
         required(:description).maybe(:string)
-        required(:file).filled(:hash)
+        required(:image_data).filled(:string)
       end
     end
   end
--- app/lib/get_img.rb
+++ app/lib/get_img.rb
@@ -8,11 +8,10 @@ class GetImg
         data = File.open(tempfile.path).read
 
         # Encode the puppy
-        encoded = Base64.encode64(data)
-        param["file"][:file] = encoded
+        encoded = Base64.strict_encode64(data)
+        param["image_data"] = encoded
 
         param["file"].delete(:tempfile)
-
         tempfile.close
         tempfile.unlink  # deletes the temp file
 
